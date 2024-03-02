ac256ca3bb4eabd8de511a49cef84e406998a341
Jim Sawicki <7102469+JamesSawicki@users.noreply.github.com>
2024-03-02 06:05:14 -0600

    32 - jdbc autoconfig finished

--- lab/32-jdbc-autoconfig/src/main/java/rewards/RewardsApplication.java
+++ lab/32-jdbc-autoconfig/src/main/java/rewards/RewardsApplication.java
@@ -2,7 +2,16 @@ package rewards;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import config.RewardsConfig;
 
 // TODO-00 : In this lab, you are going to exercise the following:
 // - Understanding how auto-configuration is triggered in Spring Boot application
@@ -34,12 +43,12 @@ import org.springframework.boot.SpringApplication;
 
 // TODO-13 (Optional) : Follow the instruction in the lab document.
 //           The section titled "Build and Run using Command Line tools".
-
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@Import(RewardsConfig.class)
 public class RewardsApplication {
     static final String SQL = "SELECT count(*) FROM T_ACCOUNT";
 
-    final Logger logger
-            = LoggerFactory.getLogger(RewardsApplication.class);
+    final Logger logger = LoggerFactory.getLogger(RewardsApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(RewardsApplication.class, args);
@@ -47,25 +56,34 @@ public class RewardsApplication {
 
     // TODO-04 : Let Spring Boot execute database scripts
     // - Move the SQL scripts (schema.sql and data.sql)
-    //   from `src/test/resources/rewards/testdb` directory
-    //   to `src/main/resources/` directory
+    // from `src/test/resources/rewards/testdb` directory
+    // to `src/main/resources/` directory
 
     // TODO-05 : Implement a command line runner that will query count from
-    //           T_ACCOUNT table and log the count to the console
+    // T_ACCOUNT table and log the count to the console
     // - Use the SQL query and logger provided above.
     // - Use the JdbcTemplate bean that Spring Boot auto-configured for you
     // - Run this application and verify "Hello, there are 21 accounts" log message
-    //   gets displayed in the console
-
+    // gets displayed in the console
+    @Bean
+    CommandLineRunner commandLineRunner(JdbcTemplate jdbcTemplate) {
+        Long accountCount = jdbcTemplate.queryForObject(SQL, Long.class);
+        return args -> logger.info("Hi, there are {} accounts in T_ACCOUNT", accountCount);
+    }
+    @Bean
+    CommandLineRunner commandLineRunner2(RewardsRecipientProperties rrp) {
+        return args ->System.out.printf("Reward Recipient is %s, age %d, a %s that likes %s", rrp.getName(), rrp.getAge(), rrp.getGender(), rrp.getHobby());
+    }
     // TODO-07 (Optional): Enable full debugging in order to observe how Spring Boot
-    //           performs its auto-configuration logic
+    // performs its auto-configuration logic
     // - Follow TO-DO-07 in application.properties, then come back here.
     // - Run the application
     // - In the console output, find "CONDITIONS EVALUATION REPORT".
-    //   It represents the auto-configuration logic used by Spring Boot.
+    // It represents the auto-configuration logic used by Spring Boot.
     // - Search for "JdbcTemplateAutoConfiguration matched:" and
-    //   "DataSourceAutoConfiguration matched:". Note that each @Conditional*
-    //   represents a single conditional statement in the "JdbcTemplateAutoConfiguration"
-    //   and "DataSourceAutoConfiguration" classes.
+    // "DataSourceAutoConfiguration matched:". Note that each @Conditional*
+    // represents a single conditional statement in the
+    // "JdbcTemplateAutoConfiguration"
+    // and "DataSourceAutoConfiguration" classes.
 
 }
