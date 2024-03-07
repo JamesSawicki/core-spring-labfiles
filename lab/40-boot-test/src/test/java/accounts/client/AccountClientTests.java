78e39760a0408af850c5b80463839be1c293e74f
Jim Sawicki <7102469+JamesSawicki@users.noreply.github.com>
2024-03-07 15:39:58 -0600

    completed 40-boot-test

--- lab/40-boot-test/src/test/java/accounts/client/AccountClientTests.java
+++ lab/40-boot-test/src/test/java/accounts/client/AccountClientTests.java
@@ -2,7 +2,13 @@ package accounts.client;
 
 import common.money.Percentage;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import rewards.internal.account.Account;
@@ -26,11 +32,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 // TODO-01: Make this class a Spring Boot test class
 // - Add @SpringBootTest annotation with WebEnvironment.RANDOM_PORT
-
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class AccountClientTests {
 
 	// TODO-02: Autowire TestRestTemplate bean to a field
 	// - Name the field as restTemplate
+	@Autowired
+	TestRestTemplate restTemplate;
 
 	// TODO-03: Update code below to use TestRestTemplate (as opposed to RestTemplate)
 	// - Remove RestTemplate from this code
@@ -44,9 +52,8 @@ public class AccountClientTests {
 	 * server URL ending with the servlet mapping on which the application can be
 	 * reached.
 	 */
-	private static final String BASE_URL = "http://localhost:8080";
+	private static final String BASE_URL = "";
 
-	private RestTemplate restTemplate = new RestTemplate();
 	private Random random = new Random();
 
 	@Test
@@ -108,11 +115,11 @@ public class AccountClientTests {
 
 		restTemplate.delete(newBeneficiaryLocation);
 
-		HttpClientErrorException httpClientErrorException = assertThrows(HttpClientErrorException.class, () -> {
-			System.out.println("You SHOULD get the exception \"No such beneficiary with name 'David'\" in the server.");
-			restTemplate.getForObject(newBeneficiaryLocation, Beneficiary.class);
-		});
-		assertThat(httpClientErrorException.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+		// HttpClientErrorException httpClientErrorException = assertThrows(HttpClientErrorException.class, () -> {
+		// 	System.out.println("You SHOULD get the exception \"No such beneficiary with name 'David'\" in the server.");
+		// 	restTemplate.getForObject(newBeneficiaryLocation, Beneficiary.class);
+		// });
+		assertThat(restTemplate.getForEntity(newBeneficiaryLocation, TestRestTemplate.class).getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
 	}
 
 	// TODO-05: Observe a log message in the console indicating
